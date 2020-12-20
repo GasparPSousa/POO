afcabe5659aaab12048d1fb9ac3f13466d08b22c
unknown <gasparufrj@gmail.com>
2020-12-20 19:08:19 -0300

    Atributos Públicos, Atributos Privados e Name Mungling

--- atributos.py
+++ atributos.py
@@ -10,6 +10,7 @@ Em Python, dividimos os atributos em 3 grupos:
     - Atributos Dinâmicos
 
 # Atributos de Instância: São atributos declarados dentro do Método Construtor.
+# Eles podem ser Públicos ou Privados
 
 OBS: Método Construtor: É um método especial para a construção do Objeto.
 
@@ -29,6 +30,7 @@ self é sempre o primeiro parâmetro.
 
 
 
+# Classes com Atributos de Instância PÚBLICO.
 class Lampada:
 
     def __init__(self, voltagem, cor):
@@ -39,7 +41,7 @@ class Lampada:
 
 # self é o próprio objeto
 # Você está falando:
-# O atribuoto voltagem do Objeto Lampada vai receber voltagem.
+# O atributo voltagem do Objeto Lampada vai receber voltagem.
 # O atributo cor do Objeto Lampada vai receber cor
 # O atributo ligada do Objeto Lampada vai receber False.
 
@@ -73,3 +75,47 @@ class Usuario:
 # O Objeto Usuario no atributo email, vai receber email.
 # O Objeto Usuário no atributo senha, vai receber senha.
 
+
+
+# Atributos Públicos ou Atributos Privados.
+# Agente está falando da VISIBILIDADE desses atributos
+# Quando vc declara um atributo como PRIVADO, ele só pode ser acessado DENTRO da própria classe que ele foi declarado.
+
+# Em Python, por convenção, ficou estabelecido que todo atributo de uma classe é PÚBLICO.
+# Ou seja, pode ser acessado em todo o projeto.
+# Caso queiramos demonstrar  que determinado atributo deve ser tratado como PRIVADO, ou seja,
+# que deve ser acessado/utilizado SOMENTE DENTRO da própria classe onde está declarado,
+# utiliza-se duplo undersore no início de seu nome.
+
+# Isso é conhecido também como Name Mangling.
+
+
+class Acesso:
+
+    def __init__(self, email, senha):
+        self.email = email
+        self.__senha = senha
+
+    def mostra_senha(self):
+        print(self.__senha)
+     # acessando atributo privado dentro da própria classe
+    def mostra_email(self):
+        print(self.email)
+
+
+# OBS: Lembre-se que isso é apenas uma convenção, ou seja, a linguagem Python não vai impedir
+# que façamos acesso aos atributos sinalizados como PRIVADOS fora da classe.
+
+# Exemplo
+
+user = Acesso('user@gmail.com', '123456')
+
+print(user.email)
+# print(user.__senha) # AtributeError - Isso é apenas um nível de segurança que Python coloca.
+print(dir(user))
+print(user._Acesso__senha) # Temos acesso, mas não deveríamos fazer esse acesso.
+# Essa junção do nome da classe com o nome do atributo privado(_Acesso__senha)  é um Name Mungling.
+
+
+user.mostra_senha()
+user.mostra_email()
