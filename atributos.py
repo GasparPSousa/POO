a529725e8ec7683b9171d4f62b845ee343ab5460
unknown <gasparufrj@gmail.com>
2020-12-20 18:10:39 -0300

    Criando Atributos de Instâncias

--- atributos.py
+++ atributos.py
@@ -13,7 +13,48 @@ Em Python, dividimos os atributos em 3 grupos:
 
 OBS: Método Construtor: É um método especial para a construção do Objeto.
 
+Médodo é uma função dentro de uma classe.
+
 
 """
 
 # Atributos de Instância:
+# Em Python, o __init__ é o Método Construtor e é nele que fazemos as declarações dos atributos de instância.
+
+# Em Python, o duplo underline __ tem o significado de privado, somente dentro da classe eu posso ter acesso.
+
+
+
+class Lampada:
+
+    def __init__(self, voltagem, cor):
+        self.voltagem = voltagem
+        self.cor = cor
+        self.ligada = False
+
+
+class ContaCorrente:
+
+    def __init__(self, numero, limite, saldo):
+        self.numero = numero
+        self.limite = limite
+        self.saldo = saldo
+
+
+class Produto:
+
+    def __init__(self, nome, descricao, valor):
+        self.nome = nome
+        self.descricao = descricao
+        self.valor = valor
+
+
+class Usuario:
+
+    def __init__(self, nome, email, senha):
+        self.nome = nome
+        self.email = email
+        self.senha = senha
+
+
+
