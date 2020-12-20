0e6964f8b8e745679dad8e07c14d57f49ae06e31
unknown <gasparufrj@gmail.com>
2020-12-20 17:43:32 -0300

    Criando o arquivo atributos.py

---
+++ atributos.py
@@ -0,0 +1,19 @@
+"""
+POO - Atributos
+
+Atributos <- Representam as características do Objeto. Ou seja, pelos atributos nós conseguimos representar
+computacionalmente os estados de um objeto.
+
+Em Python, dividimos os atributos em 3 grupos:
+    - Atributos de Instância
+    - Atributos de Classe
+    - Atributos Dinâmicos
+
+# Atributos de Instância: São atributos declarados dentro do Método Construtor.
+
+OBS: Método Construtor: É um método especial para a construção do Objeto.
+
+
+"""
+
+# Atributos de Instância:
