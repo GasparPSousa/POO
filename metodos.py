24d63e0c72f476f83c0b47be3b6efdb0ecd23682
gaspar <gasparufrj@gmail.com>
2020-12-21 14:28:51 -0300

    Definições Gerais de Métodos

--- metodos.py
+++ metodos.py
@@ -1,6 +1,11 @@
 """
+POO - Métodos
 
+- Métodos (funções) <- Representam comportamentos do objeto. Ou seja, as ações que esse objeto
+pode realizar no seu sistema.
 
-
+Em Python, dividimos os métodos em 2 grupos:
+    - Métodos de Instância
+    _ Métodos de Classe
 
 """
