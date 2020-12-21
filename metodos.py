c9d16c2c8efb2807713a912039ba3edabed8f6cf
gaspar <gasparufrj@gmail.com>
2020-12-21 14:47:39 -0300

    Criando classes e atributos

--- metodos.py
+++ metodos.py
@@ -8,4 +8,46 @@ Em Python, dividimos os métodos em 2 grupos:
     - Métodos de Instância
     _ Métodos de Classe
 
+
 """
+
+
+
+class Lampada:
+
+    def __init__(self, cor, voltagem, luminosidade):
+        self.__cor = cor
+        self.__voltagem = voltagem
+        self.__luminosidade = luminosidade
+        self.__ligada = False
+
+
+class ContaCorrente:
+
+    contador = 4999
+
+    def __init__(self, limite, saldo):
+        self.__numero = ContaCorrente.contador + 1
+        self.__limite = limite
+        self.__saldo = saldo
+        ContaCorrente.contador = self.__numero
+
+class Produto:
+
+    contador = 0
+
+    def __init__(self, nome, descricao, valor):
+        self.__id = Produto.contador + 1
+        self.__nome = nome
+        self.__descricao = descricao
+        self.__valor = valor
+        Produto.contador = self.__id
+
+
+class Usuario:
+
+    def __init__(self, nome, email, senha):
+        self.__nome = nome
+        self.__email = email
+        self.__senha = senha
+
