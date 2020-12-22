f963c9f039108d2fa40a1aa7fa986b0631de10e4
gaspar <gasparufrj@gmail.com>
2020-12-22 14:40:39 -0300

    Classes com atributos públicos

--- abstracao_encapsulamento.py
+++ abstracao_encapsulamento.py
@@ -40,3 +40,49 @@ Em POO, Abstração é o ato de expor apenas dados relevantes de uma classe, esc
 do usuário.
 
 """
+
+
+class Conta:
+
+    contador = 400
+
+    def __init__(self, titular, saldo, limite):
+        self.numero = Conta.contador
+        self.titular = titular
+        self.saldo = saldo
+        self.limite = limite
+        Conta.contador += 1
+
+    def extrato(self):
+        print(f'Saldo de {self.saldo} do titular {self.titular} com limite de {self.limite}')
+
+    def depositar(self, valor):
+        self.saldo += valor
+
+    def sacar(self, valor):
+        self.saldo -= valor
+
+
+# Testando
+
+conta1 = Conta('Gaspar', 150.00, 1500)
+
+print(conta1.numero)
+print(conta1.titular)
+print(conta1.saldo)
+print(conta1.limite)
+
+## Como o acesso está público, olha o que podemos fazer...
+
+conta1.numero = 42
+conta1.titular = 'Xuxa'
+conta1.saldo = 999999999999999
+conta1.limite = 9999999999999999999999999
+
+print(conta1.__dict__)
+
+## Logo, como o acesso está público, agente consegue não somente fazer a leitura, como
+# também podemos alterar os dados. Com isso a segurança vai por água abaixo.
+# Pq desse problema? Pois agente não encapsulou os dados conforme deveria.
+
+conta1.extrato()
