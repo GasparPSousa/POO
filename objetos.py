d7bfde30088ce2a5c568a8892ed6171109698d03
gaspar <gasparufrj@gmail.com>
2020-12-22 11:38:37 -0300

    Criando o arquivo objetos.py e Definição de Objetos

---
+++ objetos.py
@@ -0,0 +1,9 @@
+"""
+POO - Objetos
+
+Objetos <- São instâncias das classes. Ou seja, após o mapeamento do objeto do mundo real para a sua
+representação computacional, podemos criar quantos objetos forem necessários. Podemos pensar nos
+objetos/instâncias de uma classe como variáveis do tipo definido na classe.
+
+
+"""
