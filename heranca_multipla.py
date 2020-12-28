28f22e6234d297c09483432c807b480e2748f133
gaspar <gasparufrj@gmail.com>
2020-12-28 14:44:09 -0300

    Exemplos reais de Heranca Multiplas

--- heranca_multipla.py
+++ heranca_multipla.py
@@ -8,7 +8,6 @@ fazendo com que a classe filha herde todos os atributos e métodos das classes h
     - Por Multiderivação Direta;
     - Por Multiderivação Indireta;
 
-"""
 
 # Exemplo 1 - Multiderivação Direta
 
@@ -49,3 +48,65 @@ class Multiderivada(Base3):
 
 # Não importa se a derivação é direta ou indireta. A classe que realizar a herança herdará
 # todos os atributos e métodos das Super Classes
+
+"""
+
+
+class Animal:
+
+    def __init__(self, nome):
+        self.__nome = nome
+
+    def cumprimentar(self):
+        return f'Eu sou {self.__nome}'
+
+
+class Aquatico(Animal):
+
+    def __init__(self, nome):
+        super().__init__(nome)
+
+    def nadar(self):
+        return f'{self._Animal__nome} está nadando...'
+
+    def cumprimentar(self):
+        return f'Eu sou {self._Animal__nome} do mar!'
+
+
+class Terrestre(Animal):
+
+    def __init__(self, nome):
+        super().__init__(nome)
+
+    def andar(self):
+        return f'{self._Animal__nome} está andando...'
+
+    def cumprimentar(self):
+        return f'Eu sou {self._Animal__nome} da terra!'
+
+
+class Pinguim(Terrestre, Aquatico):
+
+    def __init__(self, nome):
+        super().__init__(nome)
+
+
+# Testando
+
+baleia = Aquatico('Wally')
+print(baleia.nadar())
+print(baleia.cumprimentar())
+print()
+
+tatu = Terrestre('Xim')
+print(tatu.andar())
+print(tatu.cumprimentar())
+print()
+
+tux = Pinguim('Tux')
+print(tux.andar())
+print(tux.nadar())
+print(tux.cumprimentar()) # ????? qual método cumprimentar ele vai executar?
+# Tem a ver com o Method Resolution Order - MRO
+# class Pinguim(Terrestre, Aquatico): Logo, o método cumprimentar utilizado será do método Terrestre
+# que vem primeiro que Aquático na ordem dos parâmetros da class Pinguim.
