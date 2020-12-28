30c29528237f30975d65d2ab82d1083c906d9381
gaspar <gasparufrj@gmail.com>
2020-12-28 17:19:09 -0300

    Criando o arquivo polimorfismo.py

---
+++ polimorfismo.py
@@ -0,0 +1,61 @@
+"""
+POO - Polimorfismo
+
+Poli <- Muitas
+Morfis <- Formas
+
+Objetos que podem se comportar de formas diferentes.
+
+"""
+
+class Animal(object):
+
+    def __init__(self, nome):
+        self.__nome = nome
+
+    def falar(self):
+        raise NotImplementedError('A classe filha deverá implementar esse método.')
+
+    def comer(self):
+        print(f'{self.__nome} está comendo...')
+
+
+class Cachorro(Animal):
+
+    def __init__(self, nome):
+        super().__init__(nome)
+
+    def falar(self):
+        print(f'{self._Animal__nome} fala wau wau')
+
+
+class Gato(Animal):
+
+    def __init__(self, nome):
+        super().__init__(nome)
+
+    def falar(self):
+        print(f'{self._Animal__nome} fala miau!')
+
+
+class Rato(Animal):
+
+    def __init__(self, nome):
+        super().__init__(nome)
+
+    def falar(self):
+        print(f'{self._Animal__nome} fala algo...')
+
+# Testes
+
+felix = Gato('Felix')
+felix.comer()
+felix.falar()
+
+pluto = Cachorro('Pluto')
+pluto.comer()
+pluto.falar()
+
+mickey = Rato('Mickey')
+mickey.comer()
+mickey.falar()
