b6a20e6ad748f6de38f2e2a532659afe7978729e
gaspar <gasparufrj@gmail.com>
2020-12-28 11:50:34 -0300

    Acessando atributos PRIVADOS de forma errada! Só para conhecimento

--- propriedades.py
+++ propriedades.py
@@ -17,7 +17,7 @@ class Conta:
         Conta.contador += 1
 
     def extrato(self):
-        return f'saldo de {self.__saldo} do cliente {self.__titular}'
+        return f'saldo de R$ {self.__saldo},00 do cliente {self.__titular}'
 
     def depositar(self, valor):
         self.__saldo += valor
@@ -35,4 +35,15 @@ conta2 = Conta('Cristina', 2000, 4000)
 
 print(conta1.extrato())
 print(conta2.extrato())
+
+# Caso eu queira somar os saldos das contas
+soma = conta1._Conta__saldo + conta2._Conta__saldo
+print(f'A soma do saldo das contas corrento do {conta1._Conta__titular} e da {conta2._Conta__titular} é R$ {soma},00.')
+# Mas não é correto fazer acesso dessa forma. Você NUNCA deveria fazer acesso dessa forma!
+# Só saiba que existe essa possibildiade, mas vc NUNCA deveria fazer esse acesso dessa forma num atributo PRIVADO.
+# Se está declarado PRIVADO, é porque ele deve ser acessado somente DENTRO da classe. Essa é a REGRA!
+# Para o próprio bem do sistema e bem do desenvolvedor, o ideal é que vc não faça acesse desse tipo.
+
+
+
 
